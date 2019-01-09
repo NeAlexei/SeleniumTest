8252b860694e8925fde03afffdc6fbcebca3b8c6
NeAlexei <a.nechitaylo@hypervsn.com>
2019-01-09 17:55:09 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("http://dev-platform.kino-mo.com/");
+
+        WebElement element=driver.findElement(By.xpath("//input[@name='emailid']"));
+        element.sendKeys("abc@gmail.com");
+
+        WebElement button=driver.findElement(By.xpath("//input[@name='btnLogin']"));
+        button.click();
+
+    }
+}
