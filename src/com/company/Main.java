359e3149f86491717ffa452c07ac59534f8c2e5f
NeAlexei <a.nechitaylo@hypervsn.com>
2019-01-11 12:33:34 +0300

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,10 +10,16 @@ public class Main {
         FirefoxDriver driver = new FirefoxDriver();
         driver.get("http://dev-platform.kino-mo.com/");
 
-        WebElement element=driver.findElement(By.xpath("//input[@name='emailid']"));
-        element.sendKeys("abc@gmail.com");
+//        WebElement element=driver.findElement(By.xpath("//input[@name='emailid']"));
+//        element.sendKeys("abc@gmail.com");
 
-        WebElement button=driver.findElement(By.xpath("//input[@name='btnLogin']"));
+        WebElement element=driver.findElement(By.xpath("//input[@id='input-login']"));
+        element.sendKeys("a.nechitaylo@hypervsn.com");
+
+        WebElement element=driver.findElement(By.xpath("//input[@id='input-password']"));
+        element.sendKeys("4eb@z9w7");
+
+        WebElement button=driver.findElement(By.xpath("//button[@id='loginButton']"));
         button.click();
 
     }
