10e451b75fc020b5640cef03f40fe6bc2dbb1f4c
NeAlexei <a.nechitaylo@hypervsn.com>
2019-01-11 14:45:25 +0300

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,28 @@
 package com.company;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        FirefoxDriver driver = new FirefoxDriver();
+        System.setProperty("webdriver.edge.driver","C:\\Webdrivers\\MicrosoftWebDriver.exe");
+
+        WebDriver driver = new EdgeDriver();
+
         driver.get("http://dev-platform.kino-mo.com/");
 
-//        WebElement element=driver.findElement(By.xpath("//input[@name='emailid']"));
-//        element.sendKeys("abc@gmail.com");
+        WebElement elementlog = driver.findElement(By.xpath("//input[@id='input-login']"));
+        elementlog.sendKeys("a.nechitaylo@hypervsn.com");
 
-        WebElement element=driver.findElement(By.xpath("//input[@id='input-login']"));
-        element.sendKeys("a.nechitaylo@hypervsn.com");
+        WebElement elementpas = driver.findElement(By.xpath("//input[@id='input-password']"));
+        elementpas.sendKeys("4eb@z9w7");
 
-        WebElement element=driver.findElement(By.xpath("//input[@id='input-password']"));
-        element.sendKeys("4eb@z9w7");
-
-        WebElement button=driver.findElement(By.xpath("//button[@id='loginButton']"));
+        WebElement button = driver.findElement(By.xpath("//button[@id='loginButton']"));
         button.click();
 
     }
